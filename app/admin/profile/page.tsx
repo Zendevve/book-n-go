fa9313d7f78cfb0136fe25acfa9ca1dbd41e37c9
Kramik <markexconde31@gmail.com>
2026-03-02 03:28:54 +0800

    feat: implement admin profile page with personal information, profile card, and change password components

--- app/admin/profile/page.tsx
+++ app/admin/profile/page.tsx
@@ -1,6 +1,24 @@
+import { PersonalInformation } from "@/components/personal-information"
+import { ProfileCard } from "@/components/profile-card"
+import { ChangePassword } from "@/components/ui/change-password"
+
 const ProfilePage = () => {
   return (
-    <div>page</div>
+    <div className="flex flex-1 flex-col">
+      <div className="@container/main flex flex-1 flex-col gap-2">
+        <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+          <div className="flex flex-col gap-4 px-4 md:flex-row md:items-stretch lg:px-6">
+            <div className="flex flex-1 flex-col gap-4 md:gap-6">
+              <ProfileCard />
+              <ChangePassword />
+            </div>
+            <div className="flex min-w-[420px] flex-1 flex-col [&>*]:flex-1">
+              <PersonalInformation />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
