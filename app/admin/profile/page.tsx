f4a87f5acba05b46e9cde82755d9d14b2ad990fa
Kramik <markexconde31@gmail.com>
2026-03-02 03:36:07 +0800

    fix: adjust layout for profile page to improve responsiveness and alignment

--- app/admin/profile/page.tsx
+++ app/admin/profile/page.tsx
@@ -7,8 +7,8 @@ const ProfilePage = () => {
     <div className="flex flex-1 flex-col">
       <div className="@container/main flex flex-1 flex-col gap-2">
         <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-          <div className="flex flex-col gap-4 px-4 md:flex-row md:items-stretch lg:px-6">
-            <div className="flex flex-1 flex-col gap-4 md:gap-6">
+          <div className="flex flex-wrap items-stretch gap-4 px-4 lg:px-6">
+            <div className="flex min-w-[360px] flex-1 flex-col gap-4">
               <ProfileCard />
               <ChangePassword />
             </div>
