5a5cd970a2841325475c8d26356ae7701161c606
Kramik <markexconde31@gmail.com>
2026-03-05 10:41:24 +0800

    feat: implement ProfilePage component with user profile details and change password functionality

--- app/[slug]/(main)/profile/page.tsx
+++ app/[slug]/(main)/profile/page.tsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import { PersonalInformation } from "@/components/personal-information"
+import { ProfileCard } from "@/components/profile-card"
+import { ChangePassword } from "@/components/change-password"
 
-export default function UserProfilePage() {
+const ProfilePage = () => {
   return (
-    <div>UserProfilePage</div>
+    <div className="flex flex-1 flex-col">
+      <div className="@container/main flex flex-1 flex-col gap-2">
+        <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+          <div className="flex flex-wrap items-stretch gap-4 px-4 lg:px-6">
+            <div className="flex min-w-[360px] flex-1 flex-col gap-4">
+              <ProfileCard />
+              <ChangePassword />
+            </div>
+            <div className="flex min-w-[420px] flex-1 flex-col [&>*]:flex-1">
+              <PersonalInformation />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
+
+export default ProfilePage
